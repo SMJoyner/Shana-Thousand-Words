b8cc11302285a903c880a18cfb76a6b7a6312ab6
Shana Joyner <shanatillman@Shana-Joyners-iMac.local>
2013-12-06 13:32:00 -0600

    Initial Commit

---
+++ Shana Thousand Words/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Shana Thousand Words
+//
+//  Created by Shana Joyner on 12/6/13.
+//  Copyright (c) 2013 Mind Activation Code. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "STWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([STWAppDelegate class]));
+    }
+}
